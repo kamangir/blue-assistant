a937df4df7a835c36ded6352f2be7761a9e0ef4a
kamangir <arash@kamangir.net>
2025-02-02 10:16:00 -0800

    ++ - kamangir/bolt#746

--- blue_assistant/tests/test_run_script.py
+++ blue_assistant/tests/test_run_script.py
@@ -1,9 +1,14 @@
 from blue_assistant.script.generic import GenericScript
+from blue_assistant.script.functions import load_script
 from blue_assistant import env
 
 
-def test_generic_script():
-    script = GenericScript(
+def test_run_script():
+    success, script = load_script(
         object_name=env.BLUE_ASSISTANT_TEST_OBJECT,
         verbose=True,
     )
+    assert success
+    assert isinstance(script, GenericScript)
+
+    assert script.run()
