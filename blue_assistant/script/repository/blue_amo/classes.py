a0b026f78a6c9a07a92be6fe80ffcbc59e36297d
kamangir <arash@kamangir.net>
2025-02-02 14:37:03 -0800

    ++ - kamangir/bolt#746

--- blue_assistant/script/repository/blue_amo/classes.py
+++ blue_assistant/script/repository/blue_amo/classes.py
@@ -11,12 +11,6 @@ from blue_assistant.logger import logger
 class BlueAmoScript(GenericScript):
     name = path.name(file.path(__file__))
 
-    def __init__(
-        self,
-        verbose: bool = False,
-    ):
-        super().__init__(verbose=verbose)
-
     def run(
         self,
         object_name: str,
