1897e40f9fedf6e32481ec20bbd11fee75a0f902
kamangir <arash@kamangir.net>
2025-02-04 18:39:08 -0800

    wip - kamangir/bolt#746

--- blue_assistant/script/repository/orbital_data_explorer/actions/researching_the_questions.py
+++ blue_assistant/script/repository/orbital_data_explorer/actions/researching_the_questions.py
@@ -1,6 +1,7 @@
 from blueness import module
 
 from blue_assistant import NAME
+from blue_assistant.web.crawl import crawl_list_of_urls
 from blue_assistant.script.repository.base.classes import BaseScript
 from blue_assistant.logger import logger
 
@@ -13,6 +14,14 @@ def researching_the_questions(
 ) -> bool:
     logger.info(f"{NAME}: ...")
 
+    visited_urls = crawl_list_of_urls(
+        seed_urls=script.vars["seed_urls"],
+        object_name=script.object_name,
+        max_iterations=script.nodes[node_name]["max_iterations"],
+    )
+
     logger.info("🪄")
+
+    script.nodes[node_name]["visited_urls"] = visited_urls
 
     return True
