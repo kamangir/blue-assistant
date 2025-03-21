5a48d550061640cdf190c52e4d50d6c24bb2fef7
kamangir <arash@kamangir.net>
2025-03-20 21:10:46 -0700

    pipeline fix, hopefully - kamangir/bolt#746

--- blue_assistant/RAG/input.py
+++ blue_assistant/RAG/input.py
@@ -3,13 +3,28 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
+from blueness import module
+
+from blue_assistant import NAME
+from blue_assistant.logger import logger
+
+NAME = module.name(__file__, NAME)
+
 
 def pdf_to_faiss(
-    pdf_location,
-    chunk_size=800,
-    chunk_overlap=100,
+    pdf_location: str,
+    chunk_size: int = 800,
+    chunk_overlap: int = 100,
 ):
     pdf = pypdf.PdfReader(pdf_location)
+
+    logger.info(
+        "{}.pdf_to_faiss({}): loaded {} page(s)".format(
+            NAME,
+            pdf_location,
+            len(pdf.pages),
+        )
+    )
 
     full_text = ""
     for content in pdf.pages:
@@ -18,8 +33,8 @@ def pdf_to_faiss(
 
     text_splits = RecursiveCharacterTextSplitter(
         separators="\n",
-        chunk_size=800,
-        chunk_overlap=100,
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
         length_function=len,
     ).split_text(full_text)
 
